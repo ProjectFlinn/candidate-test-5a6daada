65c0715f84139e921be1130a488c01e4eb81c8de
George Flinn <georgef13@hotmail.co.uk>
2023-11-15 22:48:05 +0000

    chore: Adjusting code styling to match format specified in .prettierrc

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,16 +5,14 @@ import styles from "./App.module.scss";
 // Character list is available in the public directory
 
 export function App() {
-  return (
-    <div className={styles.App}>
-      <header className={styles["App-header"]}>
-        <img src={logo} className={styles["App-logo"]} alt="logo" />
-        <h1 className="App-title">Lord of the Rings Character Index</h1>
-      </header>
+    return (
+        <div className={styles.App}>
+            <header className={styles["App-header"]}>
+                <img src={logo} className={styles["App-logo"]} alt="logo" />
+                <h1 className="App-title">Lord of the Rings Character Index</h1>
+            </header>
 
-      <section className="App-content">
-        {/* Lovely character list goes here */}
-      </section>
-    </div>
-  );
+            <section className="App-content">{/* Lovely character list goes here */}</section>
+        </div>
+    );
 }
